094821960232d06aa70b6af6c5082f4c42962632
Rens van Woudenberg <rensvanw@outlook.com>
2016-04-08 01:05:57 +0200

    Changed connect details

--- app/index/index.js
+++ app/index/index.js
@@ -21,6 +21,10 @@ angular.module('casusMIT3.index', ['ngRoute','AdalAngular'])
         $scope.selectedReport = $scope.reports[0].embedUrl;
     });
 
-
+    var iframe = document.getElementById("report");
+    iframe.addEventListener("load", function () {
+        var token = adal.getCachedToken("https://analysis.windows.net/powerbi/api");
+        iframe.contentWindow.postMessage(JSON.stringify({ action: "loadReport", accessToken: token }), "*");
+    });
 
 }]);;
