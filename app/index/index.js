9238cbf69f008bcb0375ea48645777e0c44c0f73
Rens van Woudenberg <rensvanw@outlook.com>
2016-04-07 18:51:25 +0200

    Added Power BI Data factory

--- app/index/index.js
+++ app/index/index.js
@@ -25,5 +25,14 @@ angular.module('casusMIT3.index', ['ngRoute','AdalAngular'])
 
 }
 
+  var iframe = document.getElementById("report");
+  iframe.addEventListener("load",function () {
+    var token = adal.getCachedToken("https://analysis.windows.net/powerbi/api");
+    iframe.contentWindow.postMessage(JSON.stringify({
+      action: "loadReport",
+      accesToken: token
+    }),
+    "*");
+  });
 
 }]);
