75c681e83fa56a3a0ba5bbb2711d6c7f419d4b37
Rens van Woudenberg <rensvanw@outlook.com>
2016-04-08 14:02:19 +0200

    Added Power BI Data factory

--- app/results/results.js
+++ app/results/results.js
@@ -207,7 +207,7 @@ angular.module('casusMIT3.results', ['ngRoute', 'AdalAngular', 'ngMaterial'])
 
         var request = new XMLHttpRequest();
 
-        request.open('POST', 'https://api.powerbi.com/v1.0/myorg/datasets?defaultRetentionPolicy=None');
+        request.open('POST', 'https://api.powerbi.com/v1.0/myorg/datasets');
 
         request.setRequestHeader('Content-Type', 'application/json');
 
